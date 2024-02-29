4b4ac9f2c5fa6191d9db6f61ba468f217ac6996f
Amereth <alek.melnyk@gmail.com>
2024-03-01 01:21:57 +0200

    restored accidentally removed lines

--- src/app/api/income/route.ts
+++ src/app/api/income/route.ts
@@ -5,7 +5,7 @@ import { env } from '@/lib/env'
 import jsonwebtoken, { JwtPayload } from 'jsonwebtoken'
 import { WithId } from 'mongodb'
 import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
 export type GetResponse = WithId<UserSchema> & { esv: EsvSchema }
 
@@ -56,4 +56,63 @@ export type PostBody = {
   monthIndex: number
   recordIndex: number
   value: number
+}
+
+export const POST = async (
+  request: NextRequest,
+): Promise<NextResponse<WithId<UserSchema> | { error: string }>> => {
+  try {
+    const jwt = cookies().get('jwt')
+
+    if (!jwt) {
+      return NextResponse.json({ error: 'unathorised' }, { status: 401 })
+    }
+
+    const decoded = jsonwebtoken.verify(jwt.value, env.JWT_SECRET)
+    const email: string = (decoded as JwtPayload).email
+
+    const body = (await request.json()) as PostBody
+    const { year, monthIndex, recordIndex, value } = body
+
+    const document = await collections.users.db.findOne({ email })
+
+    if (!document) {
+      cookies().delete('jwt')
+      throw new Error('user not found')
+    }
+
+    await collections.users.db.updateOne(
+      { email },
+      {
+        $set: {
+          [`income.${year}.${monthIndex}.${recordIndex}`]: value,
+        },
+      },
+    )
+
+    if (document.income[year][monthIndex].length - 1 === recordIndex) {
+      await collections.users.db.updateOne(
+        { email },
+        {
+          $push: {
+            [`income.${year}.${monthIndex}`]: 0,
+          },
+        },
+      )
+    }
+
+    const updatedDoc = await collections.users.db.findOne({ email })
+
+    if (!updatedDoc) {
+      cookies().delete('jwt')
+      throw new Error('user not found')
+    }
+
+    return NextResponse.json(updatedDoc, { status: 200 })
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+    return NextResponse.json({ error: 'unknown error' }, { status: 500 })
+  }
 }
